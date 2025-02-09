2f9f335e14a498018e3b184768dda8e3f11e2c7f
kshyaaa <168442668+kshyaaa@users.noreply.github.com>
2025-02-09 21:27:50 +0800

    Added a new feature: display current time and date by Kishia L. HelloGitHub.java

--- HelloGitHub.java
+++ HelloGitHub.java
@@ -1,6 +1,21 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class HelloGitHub {
     public static void main(String[] args) {
+        // By Kishia Laubenia
+
+        // Get the current date and time
+        LocalDateTime currentDateTime = LocalDateTime.now();
+
+        // Format the date and time
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDateTime = currentDateTime.format(formatter);
+        
         System.out.println("Hello GitHub!");
+
+        // Prints currrent date and time
+        System.out.println("Current Date and Time: " + formattedDateTime);
 
         
     }
